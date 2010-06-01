6151156d6505178bd33a96e6079cf71cd9685994
Clayton Lengel-Zigich <Clayton Lengel-Zigich clayton@claytonlz.com>
2010-06-01 11:14:26 -0700

    Installing redgreen

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,3 +8,4 @@ config.action_mailer.delivery_method = :test
 
 config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 config.gem 'autotest'
+config.gem 'redgreen'
